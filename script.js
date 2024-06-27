e65a498046dc22be230f19eec82298e040b72df2
ammaryasser21 <ammaryasserqwertyu@gmail.com>
2024-06-27 08:14:58 +0300

    edit some meta tages

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
-window.addEventListener('scroll', function() {
-    var nav = document.querySelector('nav');
-    var moveUpBtn = document.getElementById('moveUpBtn');
-    var navLinks = document.querySelectorAll('nav a');
-    var navButtons = document.querySelectorAll('nav button');
-    var logoImg = document.querySelector('nav img');
-    var newLogoSrc = 'assets/main-logo -2.png';  // Replace with the path to your new logo
+window.addEventListener("scroll", function () {
+  var nav = document.querySelector("nav");
+  var moveUpBtn = document.getElementById("moveUpBtn");
+  var navLinks = document.querySelectorAll("nav a");
+  var navButtons = document.querySelectorAll("nav button");
+  var logoImg = document.querySelector("nav img");
+  var newLogoSrc = "assets/main-logo -2.png"; // Replace with the path to your new logo
 
-    if (window.scrollY > 50) {
-        nav.classList.add('scroll');
-        moveUpBtn.style.display = 'block';
+  if (window.scrollY > 50) {
+    nav.classList.add("scroll");
 
-        navLinks.forEach(link => {
-            link.style.color = 'white';
-        });
+    navLinks.forEach((link) => {
+      link.style.color = "white";
+    });
 
-        navButtons.forEach(button => {
-            button.classList.add('button-scroll');
-        });
+    navButtons.forEach((button) => {
+      button.classList.add("button-scroll");
+    });
 
-        logoImg.src = newLogoSrc;
-    } else {
-        nav.classList.remove('scroll');
-        moveUpBtn.style.display = 'none';
+    logoImg.src = newLogoSrc;
+  } else {
+    nav.classList.remove("scroll");
 
-        navLinks.forEach(link => {
-            link.style.color = 'black';
-        });
+    navLinks.forEach((link) => {
+      link.style.color = "black";
+    });
 
-        navButtons.forEach(button => {
-            button.classList.remove('button-scroll');
-        });
+    navButtons.forEach((button) => {
+      button.classList.remove("button-scroll");
+    });
 
-        logoImg.src = 'assets/main-logo-1.png';
-    }
+    logoImg.src = "assets/main-logo-1.png";
+  }
+
+  if (window.scrollY > 2000) {
+    moveUpBtn.style.display = "block";
+  } else {
+    moveUpBtn.style.display = "none";
+  }
 });
 
-document.getElementById('moveUpBtn').addEventListener('click', function() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+document.getElementById("moveUpBtn").addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
-document.querySelector('.burger-menu').addEventListener('click', function() {
-    document.querySelector('.new').classList.toggle('active');
+document.querySelector(".burger-menu").addEventListener("click", function () {
+  document.querySelector(".new").classList.toggle("active");
 });

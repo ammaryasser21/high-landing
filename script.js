63db1d698b9f75e538b4bdf55e8cccc62b0d4e3c
ammaryasser21 <ammaryasserqwertyu@gmail.com>
2024-06-29 09:48:27 +0300

    edit new div and add some animation

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.addEventListener("scroll", function () {
   var navLinks = document.querySelectorAll("nav a");
   var navButtons = document.querySelectorAll("nav button");
   var logoImg = document.querySelector("nav img");
-  var newLogoSrc = "assets/main-logo -2.png"; // Replace with the path to your new logo
+  var newLogoSrc = "assets/main-logo -2.png";
 
   if (window.scrollY > 50) {
     nav.classList.add("scroll");
@@ -37,11 +37,26 @@ window.addEventListener("scroll", function () {
   } else {
     moveUpBtn.style.display = "none";
   }
-});
-
-document.getElementById("moveUpBtn").addEventListener("click", function () {
+});document.getElementById("moveUpBtn").addEventListener("click", function () {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+const links = document.querySelectorAll('.new a');
+const newDiv = document.getElementById('new');
+
 document.querySelector(".burger-menu").addEventListener("click", function () {
-  document.querySelector(".new").classList.toggle("active");
+  newDiv.classList.toggle("active");
+});
+
+function hideDiv() {
+  newDiv.classList.remove("active");
+}
+
+links.forEach(link => {
+  link.addEventListener('click', hideDiv);
+});
+
+document.addEventListener('click', function(event) {
+  if (!newDiv.contains(event.target) && !event.target.closest('.burger-menu')) {
+    hideDiv();
+  }
 });
